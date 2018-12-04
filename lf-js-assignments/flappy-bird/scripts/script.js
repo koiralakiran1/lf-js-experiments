009f331121d7e39c904b7dde69e17969beeb5a45
Kiran Koirala <koirala.kiran1@gmail.com>
2018-12-04 05:46:06 +0545

    Make randomly generated pipes move and remove them when out of screen

--- lf-js-assignments/flappy-bird/scripts/script.js
+++ lf-js-assignments/flappy-bird/scripts/script.js
@@ -1,24 +1,57 @@
 ;(function FlappyBirdGame() {
   var TOP_HEIGHT = 500;
   var FLOOR_HEIGHT = 90;
+  var gameThat = this;
 
   var topBackground = document.getElementById('top-background');
   var floorBackground = document.getElementById('floor-background');
   var mainContainer = document.getElementsByClassName('main_container');
+  topBackground.style.overflow = 'hidden';
 
   var newBird = new Bird();
-  newBird.initializeBird();
+  // newBird.initializeBird();
 
   var clickCounter = 0;
   mainContainer[0].addEventListener('click', function() {
-      // console.log('do sth');
-      if(clickCounter == 0) {
-          newBird.moveDown();
-          clickCounter++;
-      } else {
-        newBird.moveUp();
-      }
+    // console.log('do sth');
+    if (clickCounter == 0) {
+      newBird.moveDown();
+      clickCounter++;
+    } else {
+      newBird.moveUp();
+    }
   });
+
+  this.getRandomInt = function(max, min) {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  this.initiatePipes = function() {
+    this.pipeArr = [];
+    var createPipes = setInterval(function() {
+      var newPipe = new Pipe();
+      newPipe.draw();
+      pipeArr.push(newPipe);
+    }, 2000);
+  };
+
+  this.movePipe = function() {
+    var movePipeInterval = setInterval(function() {
+      gameThat.pipeArr.forEach(function(pipe) {
+        pipe.updatePipePosition();
+        //distance between pipes = (2000/10) * 2(updated Pixels) = 400;
+        if(parseInt(pipe.newTopPipeDiv.style.right) > (1300+103+17)) {
+          gameThat.pipeArr.shift();
+          pipe.newTopPipeDiv.parentNode.removeChild(pipe.newTopPipeDiv);
+          pipe.newBottomPipeDiv.parentNode.removeChild(pipe.newBottomPipeDiv);
+        }
+      });
+    }, 10);
+  };
+
+  // setInterval(function() {
+  //   console.log(gameThat.pipeArr[0].newTopPipeDiv.style.right);
+  // }, 2000);
 
   this.initiateBackground = function() {
     topBackground.style.height = TOP_HEIGHT + 'px';
@@ -34,29 +67,31 @@
     topBackground.style.backgroundRepeat = 'repeat-x';
 
     topBackground.style.position = 'relative';
-    (function moveBackground(){
-        var x = 0;
-        setInterval(function(){
-            x-=1;
-            floorBackground.style.backgroundPosition = x + 'px 0';
-            topBackground.style.backgroundPosition = x + 'px 0';
-        }, 10);
+    (function moveBackground() {
+      var x = 0;
+      setInterval(function() {
+        x -= 1;
+        floorBackground.style.backgroundPosition = x + 'px 0';
+        topBackground.style.backgroundPosition = x + 'px 0';
+      }, 10);
     })();
     // moveBackground();
   };
 
   (function() {
     initiateBackground();
+    initiatePipes();
+    movePipe();
   })();
 
 
   function Bird() {
     var birdPositionTop = 250;
     var birdThis = this;
-    this.birdDiv = document.createElement('div');
 
-    this.initializeBird = function() {
+    (function() {
       //draw part
+      this.birdDiv = document.createElement('div');
       birdDiv = document.createElement('div');
       birdDiv.style.width = '50px';
       birdDiv.style.height = '50px';
@@ -68,7 +103,7 @@
       topBackground.appendChild(birdDiv);
       birdDiv.style.top = birdPositionTop + 'px';
       birdDiv.style.left = '100px';
-    };
+    })();
 
     this.moveDown = function() {
       var moveDownInterval = setInterval(function() {
@@ -86,4 +121,44 @@
     };
 
   }
+
+  function Pipe() {
+    this.heightTop = getRandomInt(500 - 175 /*gap height*/ - 75, 75);
+    this.heightBottom = 500 - 175 - this.heightTop;
+    this.positionX = -200;
+
+    this.draw = function() {
+      //draw top
+      this.newTopPipeDiv = document.createElement('div');
+      this.newTopPipeDiv.style.width = '103px';
+      this.newTopPipeDiv.style.height = this.heightTop + 'px';
+      this.newTopPipeDiv.style.position = 'absolute';
+      this.newTopPipeDiv.style.right = this.positionX + 'px';
+      this.newTopPipeDiv.style.background = 'url("./assets/image_assets/pipe_1px.png")';
+      this.newTopPipeDiv.style.backgroundRepeat = 'repeat-y';
+      this.newTopPipeDiv.style.backgroundSize = '100%';
+      this.newTopPipeDiv.style.top = '0px';
+
+      //draw bottom
+      this.newBottomPipeDiv = document.createElement('div');
+      this.newBottomPipeDiv.style.width = '103px';
+      this.newBottomPipeDiv.style.height = this.heightBottom + 'px';
+      this.newBottomPipeDiv.style.position = 'absolute';
+      this.newBottomPipeDiv.style.right = this.positionX + 'px';
+      this.newBottomPipeDiv.style.background = 'url("./assets/image_assets/pipe_1px.png")';
+      this.newBottomPipeDiv.style.backgroundRepeat = 'repeat-y';
+      this.newBottomPipeDiv.style.backgroundSize = '100%';
+      this.newBottomPipeDiv.style.bottom = '0px';
+
+      topBackground.appendChild(this.newTopPipeDiv);
+      topBackground.appendChild(this.newBottomPipeDiv);
+    };
+
+    this.updatePipePosition = function() {
+      this.newTopPipeDiv.style.right = parseInt(this.newTopPipeDiv.style.right) + 2 + 'px';
+      this.newBottomPipeDiv.style.right = parseInt(this.newBottomPipeDiv.style.right) + 2 + 'px';
+    };
+  }
+
+
 })();
